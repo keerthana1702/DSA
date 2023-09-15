12a7e0730fa55d89a3fd38613c9f34e3888fa537
keerthana1702 <keerthanass2002@gmail.com>
2023-09-15 21:04:29 +0530

    todays update

--- array.py
+++ array.py
@@ -1,4 +1,4 @@
-# reverses the array
+print("reverses the array")
 # def rev(A, start, end):
 #     while start < end:
 #         A[start], A[end] = A[end], A[start]
@@ -10,15 +10,44 @@
 # print("Reversed list is: ",A)
 
 
-#reverse the array user input
-n = int(input())
-my_list = [input(f"Enter element {i}: ") for i in range(1, n+1)]
-print("The entered list is:", my_list)
-length = len(my_list)
-start = 0
-end = length - 1
-while start < end:
-    my_list[start], my_list[end] = my_list[end], my_list[start]
-    start += 1
-    end -= 1
-print("Reversed list:", my_list)
+print("reverse the array user input")
+# n = int(input())
+# my_list = [input(f"Enter element {i}: ") for i in range(1, n+1)]
+# print("The entered list is:", my_list)
+# length = len(my_list)
+# start = 0
+# end = length - 1
+# while start < end:
+#     my_list[start], my_list[end] = my_list[end], my_list[start]
+#     start += 1
+#     end -= 1
+# print("Reversed list:", my_list)
+
+
+
+print("find the maximum an min in the array")
+# a=[5,9,3,10,88]
+# print(max(a))
+# print(min(a))
+
+
+print("find the min and max in the array")
+# def mimx(a):
+#     a.sort()
+#     minmax = {"min": a[0], "max": a[-1]}
+#     return minmax
+# n= int(input())
+# a= [int(input({i})) for i in range(1, n+1)]
+# minmax = mimx(a)
+# print("min= ",minmax["min"])
+# print("max= ",minmax["max"])
+
+
+print("kth min and max values")
+# a= list(map(int,input().split()))
+# a= sorted(a)
+# print(a)
+# k= int(input())
+# print(a[k])
+    
+
